8215bf1806295c37eac4da3854945f11c0e399fa
Joash Christian Liwanag <nightlock0326@gmail.com>
2022-03-18 17:27:04 +0800

    Reimplemented Settings and WatchlistEditor subcommands.

--- src/args.rs
+++ src/args.rs
@@ -13,9 +13,18 @@ use std::{
 };
 
 #[derive(Parser)]
-#[clap(author,version, about, long_about = None)]
+#[clap(
+    author,
+    version, 
+    about, 
+    long_about = None,
+    after_help = "EXAMPLE:\n    nyaadle\n    nyaadle tui\n    nyaadle dl -l https://foo.bar/bar.file"
+)]
 struct Cli {
-    #[clap(short, long)]
+    #[clap(
+        short, 
+        long, 
+        help = "Parses the RSS Feed normally but does not download anything.")]
     check: bool,
 
     #[clap(subcommand)]
@@ -24,11 +33,25 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Subcommands {
-    #[clap(visible_alias = "t", subcommand_required = false)]
+    #[clap(
+        visible_alias = "t",
+        about = "Opens a terminal interface to adjust watch-lists and settings.",
+        after_help = "ALIAS:\n     t\n\nEXAMPLE:\n     nyaadle tui\n     nyaadle t --settings\n     nyaadle t -w"
+
+    )]
     Tui {
-        #[clap(short, long)]
+        #[clap(
+            short, 
+            long,
+            help = "Opens the settings TUI."
+        )]
         settings: bool,
-        #[clap(short, long)]
+
+        #[clap(
+            short, 
+            long,
+            help = "Opens the watch-list editor."
+        )]
         watchlist: bool,
     },
 
@@ -36,6 +59,7 @@ enum Subcommands {
     Download {
         #[clap(short, long, multiple_values = true)]
         links: Option<Vec<String>>,
+
         #[clap(short, long)]
         file: Option<String>,
     },
@@ -51,6 +75,51 @@ enum Subcommands {
         #[clap(short = 'o', long = "option", possible_values = [ "1080", "720", "non-vid" ])]
         vid_opt: Option<String>,
     },
+
+    #[clap(visible_alias = "set")]
+    Settings {
+        #[clap(short, long = "set-dl-dir")]
+        dl_dir: Option<String>,
+
+        #[clap(short, long = "set-ar-dir")]
+        ar_dir: Option<String>,
+
+        #[clap(short, long = "set-feed-url")]
+        url: Option<String>,
+
+        #[clap(short, long = "set-log-file")]
+        log: Option<String>,
+
+        #[clap(long = "get-dl-dir")]
+        get_dl: bool,
+
+        #[clap(long = "get-ar-dir")]
+        get_ar: bool,
+
+        #[clap(long = "get-feed-url")]
+        get_url: bool,
+
+        #[clap(long = "get-log-path")]
+        get_log: bool,
+    },
+
+    #[clap(visible_alias = "wle")]
+    WatchlistEditor {
+        #[clap(short, long)]
+        add: bool,
+        #[clap(short, long)]
+        delete: bool,
+        #[clap(short, long)]
+        edit: bool,
+        #[clap(short, long)]
+        item: Option<Vec<String>>,
+        #[clap(short = 't', long = "title")]
+        value: Option<String>,
+        #[clap(short, long)]
+        option: Option<String>,
+        #[clap(short, long)]
+        print: bool
+    }
 }
 
 pub fn args_parser() {
@@ -76,7 +145,7 @@ pub fn args_parser() {
             } else {
                 tui::main_tui();
             }
-        }
+        },
         Some(Subcommands::Download { links, file }) => {
             if let Some(urls) = links {
                 parse::arg_dl(urls);
@@ -91,7 +160,7 @@ pub fn args_parser() {
 
                 parse::arg_dl(links);
             }
-        }
+        },
         Some(Subcommands::Parse {
             feed,
             item,
@@ -103,6 +172,7 @@ pub fn args_parser() {
                 let url = settings::get_url();
                 item_parse(url, item, vid_opt);
             }
+
             fn item_parse(url: String, item_p: Option<String>, vid_opt_p: Option<String>) {
                 if let Some(title) = item_p {
                     if let Some(opt) = vid_opt_p {
@@ -115,7 +185,121 @@ pub fn args_parser() {
                     println!("Please provide an item to parse.");
                 }
             }
+        },
+        Some(Subcommands::Settings {
+            dl_dir,
+            ar_dir,
+            url,
+            log,
+            get_dl,
+            get_ar,
+            get_url,
+            get_log
+        }) => {
+            if let Some(dl) = dl_dir {
+                settings::arg_set("dl-dir", &dl);
+            } else if let Some(ar) = ar_dir {
+                settings::arg_set("ar-dir", &ar);
+            } else if let Some(url) = url {
+                settings::arg_set("url", &url)
+            } else if let Some(log) = log {
+                settings::arg_set("log", &log);
+            } else if get_dl || get_ar || get_url || get_log {
+                get_set(get_dl, get_ar, get_url, get_log);
+            } else {
+                tui::arg_tui("set");
+            }
+
+            fn get_set(dl: bool, ar: bool, url: bool, log: bool) {
+                if dl {
+                    settings::arg_get_set("dl-dir");
+                }
+                if ar {
+                    settings::arg_get_set("ar-dir");
+                }
+                if url {
+                    settings::arg_get_set("url");
+                }
+                if log {
+                    settings::arg_get_set("log");
+                }
+            }
+        },
+        Some(Subcommands::WatchlistEditor { 
+            add, 
+            delete, 
+            edit, 
+            item, 
+            value, 
+            option,
+            print
+        }) => {
+            let set_path = settings::settings_dir();
+            if add && (!delete || !edit || !print) {
+                let tgt = item_builder(value, option);
+                add_item(&set_path, tgt);
+            } else if edit && (!add || !delete || !print) {
+                let tgt = item_builder(value, option);
+                if let Some(ids) = item {
+                    for id in ids {
+                        edit_item(&set_path, &tgt, id);
+                    }
+                }
+            } else if delete && (!add || !edit || !print) {
+                if let Some(id) = item {
+                    del_item(&set_path, id);
+                }
+            } else if print && (!add || !edit || !delete) {
+                print_wl(&set_path);
+            } else {
+                tui::arg_tui("wle");
+            }
+
+            fn item_builder(val: Option<String>, opt: Option<String>) -> (String, String) {
+                if let Some(value) = val {
+                    let item_val = value;
+                    if let Some(option) = opt {
+                        let item_opt = option;
+                        (item_val, item_opt)
+                    } else {
+                        println!("Please provide both an item name and an item option.");
+                        std::process::exit(0);
+                    }
+                } else {
+                    println!("Please provide both an item name and an item option.");
+                    std::process::exit(0);
+                }
+
+            }
+
+            fn add_item(set_path: &str, item: (String, String)) {
+                settings::db_write_wl(&set_path, &item.0, &item.1).expect("Failed to write to the database.");
+                println!("Added \"{} | {}\" to the watchlist.", &item.0, &item.1);
+            }
+
+            fn edit_item(set_path: &str, item: &(String, String), id: String) {
+                settings::update_wl(set_path, &item.0, &item.1, &id).expect("Failed to write to the database.");
+                println!("Updated {} to \"{} | {}\".", id, &item.0, &item.1);
+            }
+
+            fn del_item(set_path: &str, ids: Vec<String>) {
+                for id in ids {
+                    settings::db_delete_wl(set_path, &id).expect("Failed to delete item");
+                    println!("Item deleted.");
+                }
+            }
+
+            fn print_wl(set_path: &str) {
+                let wl = settings::read_watch_list(&set_path).expect("Failed to unpack watchlist.");
+                println!("ID | Item Title | Option");
+                for item in wl {
+                let id = item.id;
+                let title = item.title;
+                let opt = item.option;
+                println!("{} | {} | {}", id, title, opt);
         }
+            }
+        },
         None => arg_check(args),
     }
 }
